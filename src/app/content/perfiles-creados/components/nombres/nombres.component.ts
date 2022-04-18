3733272fa07e458d7bde01fa5596686034103f3b
Diego Jared  Donis Soto <programacion4@recagua.com>
2022-04-18 09:39:58 -0600

    se desactivo la opcion de eliminar nombre para orgaanizacion

--- src/app/content/perfiles-creados/components/nombres/nombres.component.ts
+++ src/app/content/perfiles-creados/components/nombres/nombres.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
+import {
+	Component,
+	EventEmitter,
+	Input,
+	OnInit,
+	Output,
+	TemplateRef,
+} from '@angular/core';
 import { ApiService } from '@app/@core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { finalize } from 'rxjs/operators';
@@ -6,59 +13,66 @@ import Swal from 'sweetalert2';
 import { PUNombres } from '../../tablas-model';
 
 @Component({
-  selector: 'prx-nombres',
-  templateUrl: './nombres.component.html',
-  styleUrls: ['./nombres.component.scss'],
+	selector: 'prx-nombres',
+	templateUrl: './nombres.component.html',
+	styleUrls: ['./nombres.component.scss'],
 })
 export class NombresComponent implements OnInit {
-  @Input() nombres: PUNombres[];
-  modalRef?: BsModalRef;
-  idEditar: string;
-  btn = false;
-  @Output() actualizar = new EventEmitter();
-  constructor(private api: ApiService, private modalService: BsModalService) {}
+	@Input() nombres: PUNombres[];
+	@Input() organizacion: boolean;
+	modalRef?: BsModalRef;
+	idEditar: string;
+	btn = false;
+	@Output() actualizar = new EventEmitter();
+	constructor(private api: ApiService, private modalService: BsModalService) {}
 
-  ngOnInit(): void {}
-  eliminar(id: string) {
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: 'btn btn-danger',
-        cancelButton: 'btn btn-secondary',
-      },
-      buttonsStyling: false,
-    });
+	ngOnInit(): void {
+		console.log(this.organizacion);
+	}
+	eliminar(id: string) {
+		const swalWithBootstrapButtons = Swal.mixin({
+			customClass: {
+				confirmButton: 'btn btn-danger',
+				cancelButton: 'btn btn-secondary',
+			},
+			buttonsStyling: false,
+		});
 
-    swalWithBootstrapButtons
-      .fire({
-        title: 'Esta seguro que desea eliminar',
-        text: 'Esta accion es irreversible',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Si, Eliminalo',
-        cancelButtonText: 'Cancelar',
-        reverseButtons: true,
-      })
-      .then((result) => {
-        if (result.isConfirmed) {
-          this.btn = true;
-          this.api
-            .mantenimientosDelete('nombres', id)
-            .pipe(finalize(() => (this.btn = false)))
-            .subscribe((res: any) => {
-              if (res.error) {
-              } else {
-                swalWithBootstrapButtons.fire('Eliminado', 'Nombre Eliminado.', 'error');
-                this.actualizar.emit();
-              }
-            });
-        }
-      });
-  }
-  openModal(template: TemplateRef<any>, id?: string) {
-    if (id) {
-      this.idEditar = id;
-    }
-    this.modalRef = this.modalService.show(template);
-    this.modalService.onHide.subscribe(() => this.actualizar.emit());
-  }
+		swalWithBootstrapButtons
+			.fire({
+				title: 'Esta seguro que desea eliminar',
+				text: 'Esta accion es irreversible',
+				icon: 'warning',
+				showCancelButton: true,
+				confirmButtonText: 'Si, Eliminalo',
+				cancelButtonText: 'Cancelar',
+				reverseButtons: true,
+			})
+			.then((result) => {
+				if (result.isConfirmed) {
+					this.btn = true;
+					this.api
+						.mantenimientosDelete('nombres', id)
+						.pipe(finalize(() => (this.btn = false)))
+						.subscribe((res: any) => {
+							if (res.error) {
+							} else {
+								swalWithBootstrapButtons.fire(
+									'Eliminado',
+									'Nombre Eliminado.',
+									'error'
+								);
+								this.actualizar.emit();
+							}
+						});
+				}
+			});
+	}
+	openModal(template: TemplateRef<any>, id?: string) {
+		if (id) {
+			this.idEditar = id;
+		}
+		this.modalRef = this.modalService.show(template);
+		this.modalService.onHide.subscribe(() => this.actualizar.emit());
+	}
 }
